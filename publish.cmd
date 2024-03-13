be95bfcda69245c736c4f6c2a232287247bf812a
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2024-03-13 13:13:59 +0300

    build():Change Qt version in publish.cmd from 6.5.0 to 6.6.2

--- publish.cmd
+++ publish.cmd
@@ -9,7 +9,7 @@ mkdir %DEST_PATH%
 copy /B %RELEASE_FILE% %DEST_PATH% /Y
 
 IF "%QT_BIN%"=="" (
-  SET QT_BIN=e:\Soft\Qt\6.5.0\msvc2019_64\bin
+  SET QT_BIN=e:\Soft\Qt\6.6.2\msvc2019_64\bin
 )
 
 %QT_BIN%\windeployqt6.exe %DEST_PATH%\%DEST_FILE%
